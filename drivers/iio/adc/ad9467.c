957e9649dcd8d3af0a28fa19021c150054cbdefa
Lars-Peter Clausen <lars@metafoo.de>
2017-06-08 16:53:06 +0200

    iio: ad9467: Don't overwrite errors from the converter setup functions
    Properly pass on the error code from the converter setup functions rather
    than replacing them with EIO. This is especially important for probe
    deferral to work properly if one of the resource requested by the setup
    function is not available.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/adc/ad9467.c
+++ drivers/iio/adc/ad9467.c
@@ -1595,8 +1595,7 @@ static int ad9467_probe(struct spi_device *spi)
 	case CHIPID_AD9250:
 		ret = ad9250_setup(spi, 2, 2);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 
@@ -1608,8 +1607,7 @@ static int ad9467_probe(struct spi_device *spi)
 	case CHIPID_AD9683:
 		ret = ad9250_setup(spi, 1, 1);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 		conv->chip_info = &axiadc_chip_info_tbl[ID_AD9683];
@@ -1620,8 +1618,7 @@ static int ad9467_probe(struct spi_device *spi)
 	case CHIPID_AD9234:
 		ret = ad9680_setup(spi, 1, 1, true);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 		conv->chip_info = &axiadc_chip_info_tbl[ID_AD9234];
@@ -1637,8 +1634,7 @@ static int ad9467_probe(struct spi_device *spi)
 #endif
 		ret = ad9680_setup(spi, 1, 1, false);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 		conv->chip_info = &axiadc_chip_info_tbl[master_slave_2x_quirk ?
@@ -1650,8 +1646,7 @@ static int ad9467_probe(struct spi_device *spi)
 	case CHIPID_AD9625:
 		ret = ad9625_setup(spi);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 
@@ -1687,8 +1682,7 @@ static int ad9467_probe(struct spi_device *spi)
 	case CHIPID_AD9684:
 		ret = ad9684_setup(spi);
 		if (ret) {
-			dev_err(&spi->dev, "Failed to initialize\n");
-			ret = -EIO;
+			dev_err(&spi->dev, "Failed to initialize: %d\n", ret);
 			goto out;
 		}
 		conv->chip_info = &axiadc_chip_info_tbl[ID_AD9684];
